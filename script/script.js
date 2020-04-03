e58398236f2f007b2472ca52d9bf38944db995eb
ChloeKonde <konde.chloe@gmail.com>
2020-04-03 22:51:03 +0600

    Added sources

---
+++ script/script.js
@@ -0,0 +1,11 @@
+function getAndSet(id) {
+		if (id == 1) {
+			document.getElementById('asi').textContent = "lol)))"
+		}
+		else if (id == 2) {
+			document.getElementById('asi').textContent = "kek)))"
+		}
+		else {
+			document.getElementById('asi').textContent = ""
+		}
+	}
