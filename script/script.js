501627e375f9d8f94ee50fcfaed926e4cd109a5d
ChloeKonde <konde.chloe@gmail.com>
2020-04-10 15:50:57 +0600

    Added new sources

--- script/script.js
+++ script/script.js
@@ -1,11 +1,48 @@
-function getAndSet(id) {
-		if (id == 1) {
-			document.getElementById('asi').textContent = "lol)))"
-		}
-		else if (id == 2) {
-			document.getElementById('asi').textContent = "kek)))"
-		}
-		else {
-			document.getElementById('asi').textContent = ""
-		}
+function perc2color(perc, min = 100, max = 0) {
+	var base = (max - min);
+	
+  if (base == 0) { 
+  	perc = 100; 
+  } else {
+  	perc = (perc - min) / base * 100; 
 	}
+	
+  var r, g, b = 0;
+	if (perc < 50) {
+		r = 255;
+		g = Math.round(5.1 * perc);
+	} else {
+  	r = Math.round(510 - 5.10 * perc);
+		g = 255;
+	}
+	var h = r * 0x10000 + g * 0x100 + b * 0x1;
+	return '#' + ('000000' + h.toString(16)).slice(-6);
+}
+
+function setDesc(elem) {
+	let desc = $(elem).attr('data')
+	$('#text').text(desc + ' человек находятся в зоне на 10 мест')
+}
+
+function changeColor() {
+	let floor = $('#floorImg').attr('floor-data')
+	$.ajax({
+		url: document.location.origin + ':8000/api/getFloorData/' + floor,
+		type: 'get',
+		success: function(response) {
+			if (!response.success) return
+
+			let places = response.data;
+
+			places.forEach(place => {
+				$('#' + place.place_id).attr('fill', perc2color(place.percentage))
+				$('#' + place.place_id).attr('data', place.count)
+			})
+		}
+	});
+}
+
+$(document).ready(function() {
+	changeColor();
+	setInterval(changeColor, 10000);
+})
---
+++ server.js
@@ -0,0 +1,61 @@
+var express = require('express')
+var app = express()
+var promise = require('bluebird')
+const cors = require('cors')
+
+const initOptions = {
+	promiseLib: promise
+};
+
+const pg = require('pg-promise')(initOptions);
+
+const cn = {
+	'host': '',
+	'port': '',
+	'database': '',
+	'user': '',
+	'password': ''
+};
+
+const db = pg(cn);
+
+var bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+var HTTP_PORT = 8000
+
+app.listen(HTTP_PORT, () => {
+	console.log("Server running on %PORT%".replace("%PORT%", HTTP_PORT));
+});
+
+app.get('/api/getFloorData/:id', (req, res, next) => {
+	db.connect()
+	.then(function (obj) {
+		obj.done();
+	})
+	.catch(function (error) {
+		console.log("error:", error.message);
+		res.json( { success: false } );
+	});
+
+	floor = req.params.id;
+
+	if (['3','4'].includes(floor)) {
+		db.any(`SELECT place_id, count FROM camera_actual_data WHERE CAST(place_id AS VARCHAR(3)) LIKE '${floor}%'`)
+			.then(data => {
+				for ( const place of data ) {
+					place.count = Math.floor(Math.random() * 15 + 1);
+					place.percentage = Math.min(place.count, 10) * 10;
+				}
+				res.json( { success: true, data: data } );
+			})
+			.catch(error => {
+				console.log('Error:', error);
+				res.json( { success: false } );
+		});
+	}
+
+});
